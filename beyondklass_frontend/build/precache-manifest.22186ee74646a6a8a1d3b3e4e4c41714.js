7afeda6148692f5c06273c619446e00092584bf1
bhavik0804 <chawanbhavik@gmail.com>
2023-01-02 15:36:20 +0530

    second commit

---
+++ beyondklass_frontend/build/precache-manifest.22186ee74646a6a8a1d3b3e4e4c41714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6318c238e52b4353fcc",
+    "url": "/BeyondKlass/static/css/main.688ed213.chunk.css"
+  },
+  {
+    "revision": "f6318c238e52b4353fcc",
+    "url": "/BeyondKlass/static/js/main.f6318c23.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/BeyondKlass/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "5919aa8c7e45616cd186",
+    "url": "/BeyondKlass/static/js/2.5919aa8c.chunk.js"
+  },
+  {
+    "revision": "ae4539e8c1cc5bc9fe17",
+    "url": "/BeyondKlass/static/js/runtime~main.ae4539e8.js"
+  },
+  {
+    "revision": "20ae5c7c837295e733e50da761f91151",
+    "url": "/BeyondKlass/index.html"
+  }
+];
